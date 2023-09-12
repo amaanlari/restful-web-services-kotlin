e712de8d8f0572f83716011da0101315820b0d75
amaanlari <amaanlari@outlook.com>
2023-09-12 18:35:05 +0530

    Implemented Internationalization with i18n for different Languages

--- src/main/kotlin/com/example/restfulwebservices/hello/HelloWorldController.kt
+++ src/main/kotlin/com/example/restfulwebservices/hello/HelloWorldController.kt
@@ -1,11 +1,13 @@
 package com.example.restfulwebservices.hello
 
+import org.springframework.context.MessageSource
+import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloWorldController {
+class HelloWorldController(private val messageSource: MessageSource) {
 
     @GetMapping(path = ["/hello-world"])
     fun helloWorld(): String {
@@ -20,5 +22,16 @@ class HelloWorldController {
     @GetMapping(path = ["/hello-world/path-var/{name}"])
     fun helloWorldBeanPathVar(@PathVariable name: String): HelloWorldBean {
         return HelloWorldBean("Hello World, $name")
+    }
+
+    @GetMapping(path = ["/hello-world-internationalized"])
+    fun helloWorldInternationalized() : String?{
+        val locale = LocaleContextHolder.getLocale()
+        return messageSource.getMessage(
+            "good.morning.message",
+            null,
+            "Uh Oh! Didn't work again",
+            locale
+        )
     }
 }
